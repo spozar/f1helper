cc4149558533b806b94a113aa444131bf425d597
sandrop <sandrodanpozar@gmail.com>
2025-02-19 15:28:17 +0100

    feat: add analytics

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { Analytics } from "@vercel/analytics/remix";
+import { SpeedInsights } from "@vercel/speed-insights/remix";
 
 export const links: Route.LinksFunction = () => [
 	{ rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -50,6 +52,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				{children}
 				<ScrollRestoration />
 				<Scripts />
+				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	);
