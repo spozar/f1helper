9d7e3802e2845a762897933b34b096826c57be0f
sandrop <sandrodanpozar@gmail.com>
2025-02-22 20:49:21 +0100

    fix: move script

--- app/root.tsx
+++ app/root.tsx
@@ -51,6 +51,20 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<Meta />
 				<Links />
+				<script
+					strategy="lazyOnload"
+					data-name="BMC-Widget"
+					data-cfasync="false"
+					src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+					data-id="spozar"
+					data-description="Support F1 Calendar on Buy me a coffee!"
+					data-message=""
+					data-color="#d10f1e"
+					data-position="Right"
+					data-x_margin="18"
+					data-y_margin="18"
+					defer
+				/>
 			</head>
 			<body className="w-screen overflow-x-hidden">
 				{children}
@@ -58,18 +72,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				<Scripts />
 				<Analytics />
 				<SpeedInsights />
-				<script
-					data-name="BMC-Widget"
-					data-cfasync="false"
-					src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-					data-id="spozar"
-					data-description="Support me on Buy me a coffee!"
-					data-message=""
-					data-color="#FF5F5F"
-					data-position="Right"
-					data-x_margin="18"
-					data-y_margin="18"
-				/>
 			</body>
 		</html>
 	);
